021937c38192a3439dd01655967bc26aa1101596
fushaochen <3460707218@qq.com>
2025-03-27 14:14:54 +0800

    Initial commit

---
+++ src/main/java/org/fsc/Main.java
@@ -0,0 +1,16 @@
+package org.fsc;
+
+/**
+ * ClassName:${NAME}
+ * Package:org.fsc
+ * Description:
+ *
+ * @Author: fsc
+ * @Create: 2025/3/27 - 14:06
+ * @Version: v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
